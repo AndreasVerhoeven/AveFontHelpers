553ae36be01c29b91763887fc3f4c34a76f99867
Andreas Verhoeven <ave@aveapps.com>
2024-04-11 11:56:04 +0200

    Fixes cache to take contentSizeCategory into account

--- Sources/Font+Cache.swift
+++ Sources/Font+Cache.swift
@@ -9,14 +9,14 @@ import UIKit
 
 extension Font {
 	fileprivate static var cache = NSCache<CacheKey, UIFont>()
-	
+
 	internal func cachedOrCreatedFont() -> UIFont {
 		let cache = Self.cache
-		let key = CacheKey(self)
+		let key = CacheKey(font: self, contentSizeCategory: UITraitCollection.current.preferredContentSizeCategory)
 		if let cachedFont = cache.object(forKey: key) {
 			return cachedFont
 		}
-		
+
 		let createdFont = uncachedFont()
 		if cache.countLimit == 0 {
 			cache.countLimit = 25
@@ -24,17 +24,27 @@ extension Font {
 		cache.setObject(createdFont, forKey: key)
 		return createdFont
 	}
-	
+
 	fileprivate final class CacheKey: NSObject {
-		let key: Font
-		
-		init(_ key: Font) { self.key = key }
-		
-		override var hash: Int { return key.hashValue }
-		
+		let font: Font
+		let contentSizeCategory: UIContentSizeCategory
+
+		init(font: Font, contentSizeCategory: UIContentSizeCategory) {
+			self.font = font
+			self.contentSizeCategory = contentSizeCategory
+		}
+
+		override var hash: Int {
+			var result = 1
+			let prime = 31
+			result = prime &* result &+ font.hashValue
+			result = prime &* result &+ contentSizeCategory.rawValue.hashValue
+			return result
+		}
+
 		override func isEqual(_ object: Any?) -> Bool {
 			guard let value = object as? CacheKey else { return false }
-			return value.key == key
+			return value.font == font && value.contentSizeCategory == contentSizeCategory
 		}
 	}
 }
